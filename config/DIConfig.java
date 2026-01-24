4d7fb8843ed61986f8e1159bc4a5b9079e4cfa8f
Dadacay <158624637+Dadacay@users.noreply.github.com>
2026-01-24 21:22:51 +0800

    Update DIConfig.java

--- config/DIConfig.java
+++ config/DIConfig.java
@@ -1,1 +1,30 @@
+package config;
+
+import data.AttendanceDAO;
+import service.AttendanceService;
+
+/**
+ * Dependency Injection Configuration
+ */
+public class DIConfig {
+
+    public static void main(String[] args) {
+
+        // Create DAO
+        AttendanceDAO attendanceDAO = new AttendanceDAO();
+
+        // Inject DAO into Service
+        AttendanceService attendanceService =
+                new AttendanceService(attendanceDAO);
+
+        // Simulate application flow
+        attendanceService.markAttendance(
+                "STU101",
+                "2026-01-24",
+                "Present"
+        );
+
+        attendanceService.viewAttendance("STU101");
+    }
+}
 
